faee1e9137d91fa97d933313390054bf30925ff1
victoryeo <victoryeo1974@gmail.com>
2019-01-20 14:24:25 +0800

    remove comma

--- routes/blocks.js
+++ routes/blocks.js
@@ -1,18 +1,18 @@
 const express       = require('express')
-const bc            = require('./tinycoin.chain');
-const init          = require('./init');
+const bc            = require('./tinycoin.chain')
+const init          = require('./init')
 
 const router = express.Router();
 
 function doListing(req, { limit = 50, skip = 0} = {}) {
   return new Promise((resolve, reject) => {
-    console.log(init.blockchain);
+    console.log(init.blockchain)
     //is consensus here?
     //bc.consensus();
-    let chain_to_send = init.blockchain;
+    let chain_to_send = init.blockchain
     // Convert our blocks into dictionaries
     // so we can send them as json objects later
-    resolve(JSON.stringify(chain_to_send));
+    resolve(JSON.stringify(chain_to_send))
   })
 }
 
@@ -31,4 +31,4 @@ router.get('/', (req, res) => {
 });
 
 
-module.exports = router;
+module.exports = router
